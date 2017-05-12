28ad46ccd25bf43833a0a1626d1e1fde8fe8e7dd
Moaaz Sidat <moaaz.omer@gmail.com>
2017-05-12 15:32:21 -0400

    update flux notes

--- subjects/06-Flux/lecture.js
+++ subjects/06-Flux/lecture.js
@@ -1,29 +1,35 @@
 /*
-- Flux is an architecture, not a framework
-  - DO NOT START BUILDING STUFF WITH FLUX WHEN YOU'RE FIRST GETTING STARTED WITH REACT
-  - It can be difficult to understand why the patterns in Flux are useful if you haven't
-    already tried to solve problems w/out Flux
-  - You'll most likely hate Flux unless you're already fighting with your current JS
-    framework. If you're not, stick with what's working for you
+- Flux is an architecture introduced by Facebook, it proposes a uni-directional data flow
+  - Flux is one of the more challenging patterns to wrap your head around
+  - Don't start using Flux when getting started with ReactDOMServer
+  - It can be difficult to reason about the usefullness of the patterns in Flux if
+    you haven't already tried to solve the same problems without it.
 
-- Flux is good at:
-  - Making it easy to reason about changes to state
+  - That being said, I've found Flux to be a useful pattern when working on larger
+    applications
+  - Flux makes it easy to reason about changes to state
 
-- Remember our 2 questions:
-  - What state is there?
-  - When does it change?
+  - The 2 important questions:
+    - What state is there?
+    - When does it change?
 
-Open Flux.png
+
+Flux - data flow
+╔══════════╗     ╔════════════╗     ╔══════════╗     ╔═══════════╗
+║  Actions ║––––>║ Dispatcher ║––––>║   Store  ║––––>║   Views   ║
+╚══════════╝     ╚════════════╝     ╚══════════╝     ╚═══════════╝
+     ^                                                     │
+     └─────────────────────────────────────────────────────┘
 
 - Views
   - React components (see components)
   - Create actions (see actions/ViewActionCreators.js)
 
-- Action Creators
-  - Create "actions" with meaningful names (e.g. "load contacts", "delete contact").
-    These are the verbs. Ask yourself, "what actions can the user take?"
+- Actions
+  - What actions can the user do that will change the state?
+  - e.g. "like post", "delete comment"
+  - API requests
   - Send actions through the dispatcher
-  - Possibly trigger API requests (side effect)
 
 - Dispatcher
   - The only actual code Facebook's flux repo gives you!
